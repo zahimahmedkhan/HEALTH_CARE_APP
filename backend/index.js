78256345977ee466d014947fa83840c37b42a6e0
Zahim Ahmed Khan <zahim1632@gmail.com>
2026-01-31 00:37:42 +0500

    cors error solving..

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 const app = express();
 app.set("trust proxy", 1);
 const port = process.env.PORT || 5000;
+const FrontEnd_Url = process.env.FRONTEND_URL
 
 // Database connection
 const db = mongoose.connection;
@@ -23,12 +24,25 @@ db.once("open", () => {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+const allowedOrigins = FrontEnd_Url
+  ? FrontEnd_Url.split(",").map(url => url.trim())
+  : [];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL?.split(",") || "*" || [],
+    origin: function (origin, callback) {
+      // allow Postman / server-side requests
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS blocked for origin: ${origin}`));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  }),
+  })
 );
 
 // Health check route
